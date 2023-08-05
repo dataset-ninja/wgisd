b91e584b5270fc2c1f238e9f30869fe0cdc466b9
Ars Vladimirov <grokhael@gmail.com>
2023-08-05 21:44:21 +0600

    preview image id

--- src/settings.py
+++ src/settings.py
@@ -40,7 +40,7 @@ if RELEASE_DATE is None:
 HOMEPAGE_URL: str = "https://zenodo.org/record/3361736"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = 550503
+PREVIEW_IMAGE_ID: int = 1765003
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
 GITHUB_URL: str = "https://github.com/dataset-ninja/wgisd"
