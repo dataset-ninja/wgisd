5efc454bc3662da6157b6755bbbc23b4b5dc9930
Ars Vladimirov <grokhael@gmail.com>
2023-08-05 22:46:09 +0600

    Poster

--- src/options.py
+++ src/options.py
@@ -4,7 +4,7 @@
 
 # * Literal["ClassesPreview", "HorizontalGrid", "SideAnnotationsGrid"]
 # * If None, then preview_class will be set automatically to "ClassesPreview"
-PREVIEW_CLASS = "HorizontalGridAnimated"
+PREVIEW_CLASS = "Poster"
 
 IS_DETECTION_TASK: bool = None  # ? Set True if you want to visualize only bbox annotations
 
